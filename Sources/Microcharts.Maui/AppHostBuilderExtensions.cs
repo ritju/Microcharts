690ac57c3ccc771fca1d0f8d7981c2c8d208f240
李明阳 <noelex@outlook.jp>
2021-10-25 10:26:44 +0800

    Update SkiaSharp to 2.88.0-preview.150

--- Sources/Microcharts.Maui/AppHostBuilderExtensions.cs
+++ Sources/Microcharts.Maui/AppHostBuilderExtensions.cs
@@ -1,11 +1,11 @@
 using Microsoft.Maui.Hosting;
-using SkiaSharp.Views.Maui.Controls;
+using SkiaSharp.Views.Maui.Controls.Hosting;
 
 namespace Microcharts.Maui
 {
     public static class AppHostBuilderExtensions
     {
-        public static IAppHostBuilder UseMicrocharts(this IAppHostBuilder builder) =>
-            builder.UseSkiaSharpHandlers();
+        public static MauiAppBuilder UseMicrocharts(this MauiAppBuilder builder, bool registerRenderers, bool replaceHandlers = false) =>
+            builder.UseSkiaSharp(registerRenderers, replaceHandlers);
     }
 }
